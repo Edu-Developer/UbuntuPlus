31e41bf0fd9cda31811de046954e5cb47562a91f
Edu Developer <80501111+Edu-Developer@users.noreply.github.com>
2023-01-26 21:56:09 +0530

    Update ubuntu.sh

--- apps/ubuntu/ubuntu.sh
+++ apps/ubuntu/ubuntu.sh
@@ -2,4 +2,5 @@ SCRIPT_PATH=$(realpath ${BASH_SOURCE})
 
 sudo rm -f $SCRIPT_PATH
 
-echo "Welcome to Ubuntu in UserLAnd!"
+echo "Welcome to Ubuntu+ by Sandeep Kumar!"
+echo "Code will be added here soon for automation , enjoy!"
