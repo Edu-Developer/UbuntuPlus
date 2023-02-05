7a7651b152b9cc7b27e2690544260e6520a2e044
Edu Developer <80501111+Edu-Developer@users.noreply.github.com>
2023-02-05 17:17:37 +0530

    Checking

--- apps/ubuntu/ubuntu.sh
+++ apps/ubuntu/ubuntu.sh
@@ -13,4 +13,5 @@ echo "Updates successfully checked and installed , Have Fun!"
 echo "It is recommended to use nano for code writing for novice users."
 echo "NOTE: If this app gets stuck in starting , please reboot your phone and Try Again.."
 echo "For any queries, please contact me at sandeeprpspower@gmail.com"
+echo "TIP: Use exit command to shutdown"
 
