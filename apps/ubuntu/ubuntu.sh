d595f9654e3f6cdaf980428f2362e077259feeff
Edu Developer <80501111+Edu-Developer@users.noreply.github.com>
2023-02-26 16:23:07 +0530

    Update ubuntu.sh

--- apps/ubuntu/ubuntu.sh
+++ apps/ubuntu/ubuntu.sh
@@ -6,18 +6,24 @@ echo "Welcome to Ubuntu+ by Sandeep Kumar!"
 echo "Initializing App Updater......."
 sleep 3
 sudo apt update && sudo apt upgrade -y
-sudo apt install default-jre default-jdk vim nano gcc clang python openssh-server openssh-client -y
+sudo apt install default-jre default-jdk vim nano gcc clang python proftpd-basic-y
 sudo apt autoremove -y
+echo "Updates successfully checked and installed , Have Fun!"
+sudo service proftpd status
+sudo service proftpd stop
 clear
-echo "Finding IP"
-rm IPAddress.txt
-wget https://api.ipify.org -O IPAddress.txt -q
+rm proftpd.conf -f
+wget "https://raw.githubusercontent.com/Edu-Developer/UbuntuPlus/master/proftpd.conf" -O proftpd.conf -q
+sudo mv proftpd.conf /etc/proftpd/proftpd.conf -f
+sudo service proftpd restart
 clear
-cat IPAddress.txt
-sleep 3
-echo -e "\n Use this IP Address with your Username to connect via SSH"
+echo "-------x--------x------"
+sudo service proftpd status
+echo "-------x--------x------"
+echo "Download FTP Client(for moving file in.out) like AndFTP and connect with your username and password with : "
+echo "Hostname: 127.0.0.1"
+echo "Port: 2023"
 echo "NOTE: If this app gets stuck in starting , please reboot your phone and Try Again.."
 echo "For any queries, please contact me at sandeeprpspower@gmail.com"
 echo "TIP: Use exit command to shutdown"
-echo "Updates successfully checked and installed , Have Fun!"
 echo "It is recommended to use nano for code writing for novice users."
